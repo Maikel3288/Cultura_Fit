09ba9775473c6b08328714445736815b00638aa9
Maikel3288 <miguelcebriantoboso3@gmail.com>
2025-07-02 11:07:20 +0200

    Primer commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const fastify = require('fastify')({ logger: true });
+require('dotenv').config();
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+
+initializeApp({
+  credential: applicationDefault()
+});
+
+const db = getFirestore();
+
+const PORT = process.env.PORT || 5000;
+
+// Middleware CORS en Fastify
+fastify.register(require('@fastify/cors'), {
+  origin: '*',
+});
+
+// Rutas
+fastify.get('/', async (request, reply) => {
+  return { hello: 'hola world' };
+});
+
+
+// Iniciar servidor
+fastify.listen({ port: PORT }, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Servidor escuchando en ${address}`);
+});
---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+      <div className="p-10 bg-gray-100 min-h-screen">
+        <h1 className="text-4xl font-bold text-blue-600">¡Hola Miguel!</h1>
+        <p className="mt-2 text-lg">Tailwind está funcionando correctamente 🚀</p>
+      </div>
+    );
+  }
+  
